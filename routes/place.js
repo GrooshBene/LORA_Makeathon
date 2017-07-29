8b35e9ae6bf9bfe39b77b5bb78615e5ec3c233f4
GrooshBene <wltn000129@gmail.com>
2017-07-30 05:21:03 +0900

    File Upload Added

---
+++ routes/place.js
@@ -0,0 +1,27 @@
+function init(app, User){
+	var randomString = require('randomstring');
+	var multer = require('multer');
+	var upload = multer({
+		dest : './public/photos/',
+		rename : function(fieldname, filename){
+			return 'files_' + filename;
+		}
+	});
+	app.post('/add/photos', upload.array('photos', 5), function(req, res){
+		var photos = new User({
+			_id : randomString.generate(15),
+			thumbnail : "/photos/" + req.files[0].filename
+		});
+		photos.save(function(err, silence){
+			if(err){
+				res.send(401, "/add/photos DB Error");
+				console.log(err);
+			}
+			res.send(200, photos);
+		})
+	})
+}
+
+
+module.exports = init;
+
